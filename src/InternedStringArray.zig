1f1842398f174ecc48dc2b41ab7a6504559364bb
zivoy <ziv132@gmail.com>
2026-02-15 19:45:27 -0500

    interned string ds

---
+++ src/InternedStringArray.zig
@@ -0,0 +1,216 @@
+// idea store strings in one interned array
+// pass out gstrings for the data
+// will need a custom allocator (?) so that freeing a string will allow its space to be be reused
+// have strings be referenced counted and be in the format of
+// [users][bytes...]
+
+const std = @import("std");
+const assert = std.debug.assert;
+
+const IndexType = enum(u32) { _ };
+const LenType = u32;
+
+const StoredString = struct {
+    index: IndexType,
+    len: LenType, // not used for anything, but could be useful for serialization and debugging
+    usage: u32,
+};
+
+storage: std.ArrayList(u8),
+storedStrings: std.StringArrayHashMapUnmanaged(StoredString),
+allocator: std.mem.Allocator,
+const Interner = @This();
+
+pub fn init(allocator: std.mem.Allocator) Interner {
+    return Interner{
+        .allocator = allocator,
+        .storage = .empty,
+        .storedStrings = .empty,
+    };
+}
+
+pub fn deinit(self: *Interner) void {
+    self.storedStrings.deinit(self.allocator);
+    self.storage.deinit(self.allocator);
+}
+
+pub fn intern(self: *Interner, s: []const u8) !String {
+    assert(s.len <= String.length_max_long);
+    const len: LenType = @truncate(s.len);
+    if (len <= String.length_max_short) {
+        var content: [String.length_max_short]u8 = @splat(0);
+        @memcpy(content[0..len], s);
+
+        return .{ .len = len, .payload = .{
+            .content = std.mem.bytesToValue(String.BufferType, &content),
+        } };
+    }
+
+    // long string
+    assert(String.length_max_short < s.len);
+    var prefixArr: [String.length_prefix_long]u8 = @splat(0);
+    @memcpy(&prefixArr, s[0..String.length_prefix_long]);
+
+    // intern string
+    const index = try self.load(s);
+
+    return .{ .len = len, .payload = .{
+        .interned = .{
+            .prefix = std.mem.bytesToValue(String.PrefixType, &prefixArr),
+            .index = index,
+        },
+    } };
+}
+
+fn free(self: *Interner, s: *const String) bool {
+    if (s.len <= String.length_max_short) return true;
+
+    const str = s.slice(self.*);
+    std.debug.print("string: '{s}' len: {d}\n", .{ str, str.len });
+    const usage = self.storedStrings.getPtr(str) orelse return false;
+    usage.usage -= 1;
+    std.debug.print("Usage: {d}\n", .{usage.usage});
+    if (usage.usage == 0) {
+        const index: usize = @intFromEnum(usage.index);
+        if (!self.storedStrings.swapRemove(str)) return false;
+        @memset(self.storage.items[index .. index + s.len], undefined);
+
+        // easy case
+        if (index + s.len == self.storage.items.len) {
+            self.storage.items.len -= s.len;
+            return true;
+        }
+
+        // TODO: free from storage
+    }
+    return true;
+}
+
+fn load(self: *Interner, s: []const u8) !IndexType {
+    if (self.storedStrings.getPtr(s)) |stored| {
+        assert(s.len == stored.len);
+        stored.usage += 1;
+        return stored.index;
+    }
+
+    // TODO: replace with something that will reuse
+    const idx: IndexType = @enumFromInt(self.storage.items.len);
+    try self.storage.appendSlice(self.allocator, s);
+    errdefer self.storage.items.len -= s.len;
+    assert(s.len < String.length_max_long);
+    try self.storedStrings.put(self.allocator, s, .{
+        .index = idx,
+        .len = @truncate(s.len),
+        .usage = 1,
+    });
+    return idx;
+}
+
+pub const String = packed struct {
+    // not using the gstrings implementation because this needs to be index based
+
+    const containerSize = @sizeOf(u64);
+    const restSize = (2 * containerSize) - @sizeOf(LenType);
+    const prefixLength = restSize - @sizeOf(IndexType);
+
+    const BufferType = std.meta.Int(.unsigned, restSize * 8);
+    const PrefixType = std.meta.Int(.unsigned, prefixLength * 8);
+
+    pub const length_max_long = std.math.maxInt(LenType);
+    pub const length_prefix_long = prefixLength;
+    pub const length_max_short = restSize;
+
+    len: LenType,
+    payload: packed union {
+        content: BufferType,
+        interned: packed struct { prefix: PrefixType, index: IndexType },
+    },
+
+    pub fn deinit(
+        self: *const String,
+        interner: *Interner,
+    ) void {
+        if (!interner.free(self)) return;
+        var selfNonConst = @constCast(self);
+        selfNonConst.len = 0;
+        selfNonConst.payload = undefined;
+        selfNonConst = undefined;
+    }
+
+    pub fn eql(
+        lhs: String,
+        rhs: String,
+    ) bool {
+        if (lhs.len != rhs.len) return false;
+        if (lhs.len <= length_max_short) {
+            return lhs.payload.content == rhs.payload.content;
+        }
+        // since the strings are interned, equality is easy
+        return lhs.payload.interned.index == rhs.payload.interned.index;
+    }
+
+    pub fn order(interner: Interner, lhs: String, rhs: String) std.math.Order {
+        // check the first 8 bytes that always are present
+        const lhs_prefix = lhs.getAnyArray("payload")[0..length_prefix_long];
+        const rhs_prefix = rhs.getAnyArray("payload")[0..length_prefix_long];
+
+        const ord = std.mem.order(u8, lhs_prefix, rhs_prefix);
+        if (ord != .eq) return ord;
+
+        return std.mem.order(u8, lhs.slice(interner), rhs.slice(interner));
+    }
+
+    fn getAnyArray(self: *const String, comptime field_name: []const u8) [*]const u8 {
+        const bitOffset = comptime @bitOffsetOf(String, field_name);
+        comptime std.debug.assert(bitOffset % std.mem.byte_size_in_bits == 0);
+        const byteOffset = comptime bitOffset / std.mem.byte_size_in_bits;
+
+        return @ptrFromInt(@intFromPtr(self) + byteOffset);
+    }
+
+    /// get a slice to the string
+    pub fn slice(self: *const String, interner: Interner) []const u8 {
+        if (self.len > length_max_short) {
+            const index: usize = @intFromEnum(self.payload.interned.index);
+            return interner.storage.items[index .. index + self.len];
+        }
+
+        assert(self.len <= length_max_short);
+        const payload_ptr = self.getAnyArray("payload");
+        return payload_ptr[0..self.len];
+    }
+
+    /// get the data immediately available on the stack
+    pub fn prefix(self: *const String) []const u8 {
+        const payload_ptr = self.getAnyArray("payload");
+        const len = if (self.len > length_max_short) length_prefix_long else self.len;
+        return payload_ptr[0..len];
+    }
+};
+
+const testing = std.testing;
+
+test "interning" {
+    var i = Interner.init(testing.allocator);
+    defer i.deinit();
+
+    const string1 = try i.intern("some string 1");
+    const string2 = try i.intern("some string 2");
+    const string3 = try i.intern("some string 1");
+
+    try testing.expect(string1.eql(string3));
+    try testing.expect(!string1.eql(string2));
+
+    try testing.expectEqualStrings("some string 1", string1.slice(i));
+    try testing.expectEqualStrings("some string 2", string2.slice(i));
+    try testing.expectEqualStrings("some string 1", string3.slice(i));
+
+    try testing.expectEqualStrings("some str", string1.prefix());
+    try testing.expectEqualStrings("some str", string2.prefix());
+    try testing.expectEqualStrings("some str", string3.prefix());
+
+    string1.deinit(&i);
+    string3.deinit(&i);
+
+    std.debug.print("'{s}'\n", .{i.storage.items});
+}
--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub const gstring = @import("./gstring.zig");
 pub const StableIndexArray = @import("stableIndexArray.zig").StableIndexArray;
+pub const InternedStringArray = @import("InternedStringArray.zig");
 
 test {
     std.testing.refAllDeclsRecursive(@This());
